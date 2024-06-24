153b289c7879abb90229905bdd1ae2162b024a8c
Kostadina Geceska <kostadinag@nextsense.com>
2024-06-24 14:28:56 +0200

    initail commmit

---
+++ World.framework/Headers/World.h
@@ -0,0 +1,18 @@
+//
+//  World.h
+//  World
+//
+//  Created by Kostadina Gecevska on 24.6.24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for World.
+FOUNDATION_EXPORT double WorldVersionNumber;
+
+//! Project version string for World.
+FOUNDATION_EXPORT const unsigned char WorldVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <World/PublicHeader.h>
+
+
---
+++ World/WorldGreetings.swift
@@ -0,0 +1,19 @@
+//
+//  WorldGreetings.swift
+//  World
+//
+//  Created by Kostadina Gecevska on 24.6.24.
+//
+
+import Foundation
+
+public class WorldGreetings {
+    let hello = "Hello"
+
+    public init() {}
+
+    public func hello(to whom: String) -> String {
+        return "Hello \(whom)"
+    }
+}
+
